e8fffffcb09c96451e419e066ce73ddeffeb3c8f
Veilza <113429647+Veilza@users.noreply.github.com>
2026-05-11 17:50:29 -0500

    Typo'd the router, and also update build.yml to use v4 actions

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 // Main Components
 import OverviewComponent from '@/components/Overview/overview.vue'
-import ModifiersComponent from '@/components/modifiers/modifiers.vue'
+import ModifiersComponent from '@/components/Modifiers/modifiers.vue'
 import ApiComponent from '@/components/API/api.vue'
 import ChangelogComponent from '@/components/Changelog/changelog.vue'
 import NotFoundComponent from '@/components/NotFound/notfound.vue'
